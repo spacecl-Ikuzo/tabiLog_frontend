c1110c5561c2a2a61daa5f459dec9973c2e8b1d9
shjeong99 <mcncshjeong@gmail.com>
2025-09-04 11:59:13 +0900

    api 통신, store 부분 수정

--- src/components/layout/top-navigation.tsx
+++ src/components/layout/top-navigation.tsx
@@ -9,7 +9,7 @@ import { useNavigate } from 'react-router-dom';
 import useUserStore from '../../store';
 import { toast } from 'sonner';
 export default function TopNavigation() {
-  const { email } = useUserStore();
+  const { userId } = useUserStore();
   const navigate = useNavigate();
   const handleLogout = () => {
     axiosInstance.post('/auth/logout');
@@ -22,7 +22,7 @@ export default function TopNavigation() {
       <div className="flex justify-end items-center w-full mr-8">
         <DropdownMenu>
           <DropdownMenuTrigger className="!focus-visible:ring-0 bg-stone-900 p-2 rounded-sm text-stone-100 hover:bg-stone-900/80">
-            {email.split('@')[0]} 님
+            {userId.split('@')[0]} 님
           </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuItem onClick={handleLogout}>로그아웃</DropdownMenuItem>
