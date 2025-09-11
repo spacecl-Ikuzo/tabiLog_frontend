1b75d475ac887c8b551b6ec675dd6dc7b5e70121
shjeong99 <mcncshjeong@gmail.com>
2025-09-11 20:21:09 +0900

    여행 리스트 수정, 스켈레톤, 캘린더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import PrivateRoute from './PrivateRoute';
 import Home from './pages/home/Home';
 import Register from './pages/register/Register';
 import Plans from './pages/plans/Plans';
-import PlanDetail from './pages/plans/PlanDetail';
+import PlanMobile from './pages/plans/PlanMobile';
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         {/* 로그인 유저만 접근 가능한 페이지들을 여기에 추가 */}
         {/* 예시: <Route path="/profile" element={<Profile />} /> */}
         <Route path="/plans" element={<Plans />} />
-        <Route path="/plans/:id" element={<PlanDetail />} />
+        <Route path="/plans/:id" element={<PlanMobile />} />
       </Route>
     </Routes>
   );
