0c13f08bad217c8bac041b23eee35638e609a666
shjeong99 <mcncshjeong@gmail.com>
2025-09-11 19:12:24 +0900

    여행 계획 리스트 페이징 처리

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Login from './pages/login/Login';
 import PrivateRoute from './PrivateRoute';
 import Home from './pages/home/Home';
 import Register from './pages/register/Register';
+import Plans from './pages/plans/Plans';
+import PlanDetail from './pages/plans/PlanDetail';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Route element={<PrivateRoute />}>
         {/* 로그인 유저만 접근 가능한 페이지들을 여기에 추가 */}
         {/* 예시: <Route path="/profile" element={<Profile />} /> */}
+        <Route path="/plans" element={<Plans />} />
+        <Route path="/plans/:id" element={<PlanDetail />} />
       </Route>
     </Routes>
   );
