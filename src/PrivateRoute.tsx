6d9eef014d1335021429436deeb7fca0b9f8fded
shjeong99 <mcncshjeong@gmail.com>
2025-09-15 11:52:48 +0900

    store  토큰을 가지고 오게 변경

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
+import useUserStore from './store';
 
 const PrivateRoute = () => {
   // localStorage에서 토큰 확인
-  const accessToken = localStorage.getItem('accessToken');
-  
+  const accessToken = useUserStore.getState().token;
+
   // 토큰이 있으면 보호된 라우트에 접근 허용
   if (accessToken) {
     return <Outlet />;
   }
-  
+
   // 토큰이 없으면 로그인 페이지로 리다이렉트
   return <Navigate to="/login" replace />;
 };
